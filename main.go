7fe14aa9714fd905bdd26f2e78b839a703b14aa8
Tom Booth <tombooth@gmail.com>
2014-08-31 16:14:55 +0100

    Pass through a directory for your API
    This will ensure the directory exists, or use a prexisting one. There
    are questions and some logging about what api-for-schema is upto.

--- main.go
+++ main.go
@@ -1,18 +1,59 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"path"
 
 	"github.com/docopt/docopt-go"
 	"github.com/tombooth/api-from-schema/schematic"
 )
 
+func question(format string, a ...interface{}) error {
+	var answer string
+
+	fmt.Printf(
+		"%v, are you sure you want to continue? (y/n) ",
+		fmt.Sprintf(format, a...),
+	)
+	if _, err := fmt.Scanf("%s", &answer); err != nil {
+		return err
+	} else if answer != "y" {
+		return errors.New(fmt.Sprintf("Aborted with answer %v", answer))
+	}
+
+	return nil
+}
+
+func ensureDirectory(projectPath string) (string, error) {
+	cleanedPath := path.Clean(projectPath)
+
+	if finfo, err := os.Stat(cleanedPath); err != nil && !os.IsNotExist(err) {
+		return "", err
+	} else if finfo != nil && !finfo.IsDir() {
+		return "", errors.New(fmt.Sprintf("%v is a file not a directory", projectPath))
+	} else if finfo != nil && finfo.IsDir() {
+		if err := question("%v exists", projectPath); err != nil {
+			return "", err
+		}
+	} else {
+		if err := question("Creating directory %v", projectPath); err != nil {
+			return "", err
+		}
+		if err := os.MkdirAll(cleanedPath, 0755); err != nil {
+			return "", err
+		}
+	}
+
+	return cleanedPath, nil
+}
+
 func main() {
 	usage := `Api from schema
 
 Usage:
-  api-from-schema [options] <json_schema>
+  api-from-schema [options] <json_schema> <project_path>
   api-from-schema -h | --help
   api-from-schema --version
 
@@ -23,7 +64,7 @@ Options:
 `
 
 	arguments, _ := docopt.Parse(usage, nil, true, "Api from schema 0.1.0", false)
-	path := arguments["<json_schema>"].(string)
+	schemaPath := arguments["<json_schema>"].(string)
 	templateStore, err := NewTemplateStore(arguments["--templates"].(string))
 
 	if err != nil {
@@ -31,7 +72,13 @@ Options:
 		return
 	}
 
-	if apiSchema, err := schema.ParseSchema(path); err == nil {
+	projectPath, err := ensureDirectory(arguments["<project_path>"].(string))
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to ensure project directory: %v", err)
+	}
+
+	if apiSchema, err := schema.ParseSchema(schemaPath); err == nil {
 		apiSchema.Resolve(nil)
 		models := ModelsFromSchema(apiSchema)
 
@@ -46,7 +93,14 @@ Options:
 		if apiOutput, err := templateStore.ExecuteAndFormat(context, "api.tmpl", "handlerfunc.tmpl"); err != nil {
 			fmt.Fprintf(os.Stderr, "Failed to execute api template: %v", err)
 		} else {
-			fmt.Fprint(os.Stdout, apiOutput)
+			mainPath := path.Join(projectPath, "main.go")
+
+			if mainFile, err := os.Create(mainPath); err != nil {
+				fmt.Fprintf(os.Stderr, "Failed to open main.go to write into: %v", err)
+			} else {
+				fmt.Fprintf(os.Stdout, "Writing api to %v", mainPath)
+				fmt.Fprint(mainFile, apiOutput)
+			}
 		}
 	}
 }
